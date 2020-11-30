ca6e54a861cfb64a1d16ab31381483efffdbc2e3
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2020-11-29 21:20:17 -0300

    Use environment variable for public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuetify-material-auth/'
-    : '/',
+  publicPath: process.env.PUBLIC_PATH || '/',
 
   'transpileDependencies': [
     'vuetify'
